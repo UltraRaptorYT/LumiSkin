a5952a10283d72fced1ecbc2e07935287cd998b6
Soh Hong Yu <raymondng009@gmail.com>
2024-12-25 22:19:31 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -134,7 +134,7 @@ export default function Home() {
         width={720}
         videoConstraints={videoConstraints}
         mirrored={true}
-        className="rounded-lg mx-auto aspect-[300/450] md:w-full w-[300px] h-[450px] object-cover"
+        className="rounded-lg mx-auto aspect-[300/450] w-full object-cover"
       />
       <button onClick={capture}>Capture photo</button>
     </div>,
