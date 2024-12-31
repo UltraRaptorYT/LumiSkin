1ac183dc2fafab1074e0844a48706ccfc0800a6d
Raymond <raymondng009@gmail.com>
2024-12-31 12:22:34 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,13 @@
 import { rewrite } from "@vercel/edge";
 
+export const config = {
+  matcher: "/api/:path*",
+};
+
 export default function middleware(request: Request) {
   const url = new URL(request.url);
   console.log(url);
-  if (url.pathname.startsWith("/about")) {
-    return rewrite(new URL("/about-2", request.url));
-  }
-
-  if (url.pathname.startsWith("/dashboard")) {
-    return rewrite(new URL("/dashboard/user", request.url));
+  if (url.pathname.startsWith("/api")) {
+    return rewrite(new URL("/api-2", request.url));
   }
 }
