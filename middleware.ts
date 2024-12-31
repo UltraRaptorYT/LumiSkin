fe91c4327ee8ff70d25286d55cac04696037e79a
Raymond <raymondng009@gmail.com>
2024-12-31 12:17:43 +0800

    Change to Edge Functions

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { rewrite } from "@vercel/edge";
+
+export default function middleware(request: Request) {
+  const url = new URL(request.url);
+
+  if (url.pathname.startsWith("/about")) {
+    return rewrite(new URL("/about-2", request.url));
+  }
+
+  if (url.pathname.startsWith("/dashboard")) {
+    return rewrite(new URL("/dashboard/user", request.url));
+  }
+}
